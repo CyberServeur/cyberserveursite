ab2babfd90e46f44b138178c2be058ed80e4fa31
Jean-François Morneau <jeanfrancois.morenau@gmail.com>
2011-06-22 12:40:07 -0400

    Traduction Francais 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyberserveursite::Application.config.secret_token = 'f9f8b8df1268103611c702b1b695386cfb7734df607435929bc1f3b00201098a9bc813b76f5dd6b0f389266dbe633ce081d90c7fa0f3917c2d20a4e85b372979'
